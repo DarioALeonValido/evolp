b23460213bed9abb19edc40d2d9ed2c681300bde
Dario <dalv.cuba@gmail.com>
2020-10-07 18:10:58 +0200

    producing some figures

--- PCA_alzheimer/alzheimer.py
+++ PCA_alzheimer/alzheimer.py
@@ -22,8 +22,10 @@ from os.path import *
 
 # General variables ------------------------------------------------------
 
+lite_version = True
+Ngen = 1000
 Npc = 20 #the maximum possible number of PCs is the number of genes
-sample_id = 'FWM'
+sample_id = 'FWM' # ['FWM' 'HIP' 'PCx' 'TCx']
 
 datapath = '../databases_external/Aging_Brain/gene_expression/'
 donor_file = 'DonorInformation.csv'
@@ -75,22 +77,23 @@ def read_samples(datapath,samples_file,sample_id,ND_id,AD_id):
 
 def read_expression(datapath,fpkm_file,index_ND,index_AD):
     with open(datapath+fpkm_file) as efile:
-        e_csv = csv.reader(efile,delimiter=',')
+        e_csv = csv.reader(efile)
+        headers = next(e_csv)
 
         data = []
-        for c in np.concatenate((index_ND,index_AD)):
+        genes_id = []
+        for row in e_csv:
             data.append([])
-            print('len',len(data))
-            for row in e_csv:
+            genes_id.append(row[0])
+            for c in np.concatenate((index_ND,index_AD)):
                 data[-1].append(row[c])
-        print('one element', data[0][0])
-        print('one element', data[1][1])
-        data = np.array(data, np.float) + 0.1
-        #data = np.array(data) + 0.1
-        ref = gmean(data[:len(index_ND)-1])
-        data = np.log2(data/ref)
 
-    return data
+    data = np.array(data, dtype="f")
+    data = data.T + 0.1
+    ref = gmean(data[:len(index_ND)])
+    data = np.log2(data/ref)
+
+    return data,genes_id
 
 
 # Reading and processing the data ------------------------------------------
@@ -98,14 +101,24 @@ def read_expression(datapath,fpkm_file,index_ND,index_AD):
 print("Reading files from databases...")
 ND_id,AD_id = read_donor(datapath,donor_file)
 index_ND,index_AD = read_samples(datapath,samples_file,sample_id,ND_id,AD_id)
-data = read_expression(datapath,fpkm_file,index_ND,index_AD)
+data,genes_id = read_expression(datapath,fpkm_file,index_ND,index_AD)
 len_ND = len(index_ND)
 len_AD = len(index_AD)
 print('Number of '+sample_id+' samples:', len_ND+len_AD)
 print('Number of ND cases:', len_ND)
 print('Number of AD cases:', len_AD)
-print('Number of genes:', len(data[0]))
+print('Number of genes:', len(genes_id))
 print("Data successfully loaded!")
+
+# reducing the data by Ngen genes for Lite version
+if(lite_version and data.shape[1] > Ngen):
+    print("Lite version: reducing the number of genes by", Ngen)
+    data = data[:, :Ngen]
+    genes_id = genes_id[:Ngen]
+elif(len(data) < Ngen):
+    lite_version = False
+    print("Warning: Imposible reduction, number of genes larger than databases.")
+    print("The Full version will be performe instead.")
 
 print("Computing PCA components...")
 eigenvalues,eigenvectors,eigenvalues_normalized,projection = pca.PCA_core(data)
@@ -130,27 +143,25 @@ np.savetxt(sample_id+'eval_dat.dat', eigenvalues, fmt='%f')
 np.savetxt(sample_id+'evec-t_dat.dat', eigenvectors, fmt='%f')
 np.savetxt(sample_id+'ind20_dat.dat', index, fmt='%i')
 np.savetxt(sample_id+'pc20_dat.dat', components, fmt='%f')
-np.savetxt(sample_id+'ngenes_dat.dat', np.transpose([index,gene[index],components]), delimiter="\t", fmt="%s")
+#np.savetxt(sample_id+'ngenes_dat.dat', np.transpose([index,genes_id[index],components]), delimiter="\t", fmt="%s")
 
 fig1, ax1 = plt.subplots()
 fig2, ax2 = plt.subplots()
     
-ax1.scatter(-projection[0,[0,1,2,3]], -projection[1,[0,1,2,3]], c='b',s=25,label="Ara+(anc)")
-ax1.scatter(-projection[0,[8,9,10,11]],-projection[1,[8,9,10,11]],s=25,label="Ara-(anc)",facecolors='none',edgecolors='b')
-ax1.scatter(-projection[0,[4,5,6,7]],-projection[1,[4,5,6,7]], c='r',s=25,label="Ara+(evol)")
-ax1.scatter(-projection[0,[12,13,14,15]],-projection[1,[12,13,14,15]],s=25,label="Ara-(evol)",facecolors='none',edgecolors='r')
-ax1.plot(-center_pc1_anc+radius_pc1_anc*np.cos(theta),-center_pc2_anc+radius_pc2_anc*np.sin(theta),alpha=0.7)
-ax1.plot(-center_pc1_evol+radius_pc1_evol*np.cos(theta),-center_pc2_evol+radius_pc2_evol*np.sin(theta),alpha=0.7)
+ax1.scatter(-projection[0,range(len_ND)], projection[1,range(len_ND)], c='b',s=25,label="ND "+sample_id)
+ax1.scatter(-projection[0,range(len_AD)], projection[1,range(len_AD)], c='r',s=25,label="AD "+sample_id)
+ax1.plot(center_pc1_ND+radius_pc1_ND*np.cos(theta),center_pc2_ND+radius_pc2_ND*np.sin(theta),alpha=0.7)
+ax1.plot(center_pc1_AD+radius_pc1_AD*np.cos(theta),center_pc2_AD+radius_pc2_AD*np.sin(theta),alpha=0.7)
 
 for i in index:
     ax2.plot([i,i],[0, eigenvectors[i,0]], color='k')
 
-ax1.set_title('Ara'), ax1.grid(), ax1.set_xlabel('PC1'), ax1.set_ylabel('-PC2'), ax1.legend()
-ax2.set_title('Ara'), ax2.grid(), ax2.set_xlabel('Genes'), ax2.set_ylabel('PC1')
+ax1.set_title('Alzheimer'), ax1.grid(), ax1.set_xlabel('PC1'), ax1.set_ylabel('-PC2'), ax1.legend()
+ax2.set_title('Alzheimer'), ax2.grid(), ax2.set_xlabel('Genes'), ax2.set_ylabel('PC1')
 fig1.tight_layout() 
 fig2.tight_layout() 
       
-fig1.savefig('_PC2_vs_PC1_fig.png')
-fig2.savefig("_PC1_fig.png")
+fig1.savefig(sample_id+'_PC2_vs_PC1_fig.png')
+fig2.savefig(sample_id+"_PC1_fig.png")
 
 print("Done!")
