8f44b5ef225b73e06fc660f1a428d00cbf374042
Dario <dalv.cuba@gmail.com>
2020-11-26 14:22:48 +0100

    exporting results from the random procedure

--- Entropy_tumors/entropy.py
+++ Entropy_tumors/entropy.py
@@ -3,242 +3,190 @@ This script file computes the entropy and overlap of PCA.
 The procedure and main results are described in paper 
 arXiv:2005.02271
 For more details on this file see author(s):
-FEQ
+FQ, DALV
 """
 
 import numpy as np
 import math, pylab
 import xlrd as xl
-from scipy.stats.mstats import gmean
 import matplotlib.pyplot as plt
 import sys,os
-
+import random
 
 # General variables ------------------------------------------------------
 
+Npc = 20
+tissues_id = ['BRCA','COAD','HNSC','KIRC','KIRP','LIHC','LUAD','LUSC','PRAD','STAD','THCA','UCEC']
+N_random = 1000
+
 srcpc = "../databases_generated/TCGA_pca/"
 srcsampl = "../databases_external/TCGA/"
+sample_path = '../databases_external/TCGA/'
+PC_path = '../databases_generated/TCGA_pca/'
 
-# Functions ------------------------------------------------------
-def read_data(srcpc,srcsampl):
-    data={}
-    pcfiles = os.listdir(srcpc)
-    samplefiles = os.listdir(srcsampl)
-    samplname = [f[+6:-4] for f in samplefiles if f[-3:] == 'xls']
-    pcname = [f[+2:-4] for f in pcfiles if f[-3:] == 'xls']
-    missmatch_pc_samples=list(set(pcname) - set(samplname))
-    for name in samplname:
-        if (name not in missmatch_pc_samples):
-            data[name]={}
-            data[name]['normal'], data[name]['tumor']=[],[]
-            wbpc,wbsampl = xl.open_workbook(srcpc+"pc"+name+".xls"),xl.open_workbook(srcsampl+"sample"+name+".xls")
-            wspc,wssampl = wbpc.sheet_by_index(0),wbsampl.sheet_by_index(0)
-            minrows=min(wssampl.nrows,wspc.nrows)
-            for row in range(1,minrows):
-                if (wssampl.cell_value(row,3)=='Solid Tissue Normal'):
-                    data[name]['normal'].append(wspc.row_values(row-1))
-                else:
-                    data[name]['tumor'].append(wspc.row_values(row-1))
-    return data, missmatch_pc_samples
 
-def _transpose_data(data):
-    core_matrix = {}
-    for label in data.keys():
-        core_matrix[label] = {}
-        for stage, core_matrix_T in data[label].items():
-            core_matrix[label][stage] = np.around(np.array(core_matrix_T, dtype=np.float64).T, decimals = 10)
-    return core_matrix
+# Functions ------------------------------------------------
 
-def _low_dimension_core_matrix(core_matrix, LOW_DIMENSION):
-        unif_restricted_core_matrix={}
-        for label in core_matrix.keys():
-            unif_restricted_core_matrix[label]={}
-            for stage in core_matrix[label].keys():
-                unif_restricted_core_matrix[label][stage]=[]
-                for i in range(LOW_DIMENSION):
-                    row=core_matrix[label][stage][i]
-                    unif_restricted_core_matrix[label][stage].append(row)
-        return unif_restricted_core_matrix
+def read_PC(sample_path,PC_path,tissue_id,Npc):
+    pc_normal = []
+    pc_tumor = []
+    sample_wb = xl.open_workbook(sample_path+"sample"+tissue_id+".xls")
+    sample_ws = sample_wb.sheet_by_index(0)
+    pc_wb = xl.open_workbook(PC_path+"pc"+tissue_id+".xls")
+    pc_ws = pc_wb.sheet_by_index(0)
+    print('Total of',tissue_id+' cases:', sample_ws.nrows)
+    for i in range(sample_ws.nrows-1):
+        if(sample_ws.cell_value(i+1,3) == 'Solid Tissue Normal'):
+            pc_normal.append([])
+            for j in range(Npc):    
+                pc_normal[-1].append(pc_ws.cell_value(i,j))
+        else:
+            pc_tumor.append([])
+            for j in range(Npc):    
+                pc_tumor[-1].append(pc_ws.cell_value(i,j))
 
-def _PC_pair_plot(core_matrix, i, j,location_name):
-    PC_j={}
-    PC_i={}
-    names = []
-    for label in core_matrix.keys():
-        names.append(label)
-        PC_i[label]={}
-        PC_j[label]={}
-        for stage in core_matrix[label].keys():
-            PC_i[label][stage]=core_matrix[label][stage][i-1]
-            PC_j[label][stage]=core_matrix[label][stage][j-1]
-        
-    fig, ax = plt.subplots()
-    ax.scatter(PC_i[location_name]['normal'], PC_j[location_name]['normal'], label='normal')
-    ax.scatter(PC_i[location_name]['tumor'],PC_j[location_name]['tumor'], label='tumor')
-    
-    plt.xlabel("pc_{0}".format(i))
-    plt.ylabel("pc_{0}".format(j))
-    plt.title("pc_map_{0}".format(location_name))
-    pylab.legend(loc='lower left')
-    plt.tight_layout()
-    plt.savefig('pairplot_fig.pdf')   
-    return None
+    pc_normal = np.array(pc_normal, dtype=np.float64)
+    pc_tumor  = np.array(pc_tumor , dtype=np.float64)
 
-def _std_dict(core_matrix):
-    std={}
-    for label in core_matrix.keys():
-        std[label]={}
-        for stage in core_matrix[label].keys():
-            std[label][stage]=[]
-            for row in core_matrix[label][stage]:
-                std[label][stage].append(np.std(row))
-    return std
+    return pc_normal, pc_tumor
 
-def _mean_dict(core_matrix):
-    mean={}
-    for label in core_matrix.keys():
-        mean[label]={}
-        for stage in core_matrix[label].keys():
-            mean[label][stage]=[]
-            for row in core_matrix[label][stage]:
-                mean[label][stage].append(np.mean(row))
-    return mean
 
-def _covariance_matrix_(core_matrix):
-    covariance_matrix={}
-    for label in core_matrix.keys():
-        covariance_matrix[label]={}
-        for stage in core_matrix[label].keys():
-            covariance_matrix[label][stage]=\
-            np.cov(core_matrix[label][stage], ddof=1 )
-    
-    return covariance_matrix
+def compute_entropy(pc_dataset):
 
-def _matrix_SVD(covariance_matrix):
-    s, cov_inv = {},{}
-    for label in covariance_matrix.keys():
-        s[label], cov_inv[label] = {},{}
-        for stage in covariance_matrix[label].keys():
-            u, ss, vh =np.linalg.svd(covariance_matrix[label][stage])
-            s[label][stage]=np.diag(ss)
-            cov_inv[label][stage]=np.matmul(np.matmul(vh.T,np.linalg.inv(s[label][stage])),u.T)
-    return s, cov_inv
+    coreM=pc_dataset.T
+    cov_mat=np.cov(coreM,ddof=1)
+    mean=np.mean(coreM,axis=1)
+    stdev=np.std(coreM,axis=1)
+    u,ss,vh =np.linalg.svd(cov_mat)
+    s=np.diag(ss)
+    cov_inv=np.matmul(np.matmul(vh.T,np.linalg.inv(s)),u.T)
+    sum_log=sum(math.log(s[i][i]) for i in range(Npc))
+    S_dataset=0.5*(sum_log + Npc*(1 + math.log(2*math.pi)))
 
-def _H_multiv_gaussian(covariance_matrix,DIMENSION):
-    H_={}
-    S_, cov_inv = _matrix_SVD(covariance_matrix)
-    for label in covariance_matrix.keys():
-        H_[label]={}
-        for stage in covariance_matrix[label].keys():
-            sum_log = sum(math.log(S_[label][stage][i][i]) for i in range(DIMENSION))
-            H_[label][stage] =\
-            0.5*(sum_log + DIMENSION*(1 + math.log(2*math.pi) ))   
-    return H_
+    return S_dataset,mean,cov_mat,cov_inv
 
-def _delta_H(entropy_dict):
-    delta_H={}
-    for label in entropy_dict.keys():
-        delta_H[label]= entropy_dict[label]['tumor']\
-                       -entropy_dict[label]['normal']
-    return delta_H                   
 
-def _overlap(mean, covariance_matrix, DIMENSION):
-    overlap, log_overlap={},{}
-    S, Cov_Inv=_matrix_SVD(covariance_matrix)
-    A, B = DIMENSION/2 , DIMENSION/4
-    for label in mean.keys():
-        inv_Vc = Cov_Inv[label]['normal']+Cov_Inv[label]['tumor']
-        Vc = np.linalg.inv(inv_Vc)
+def compute_overlap(Npc,mean_n,mean_t,cov_mat_n,cov_mat_t,cov_inv_n,cov_inv_t):
+
+    A, B = Npc/2 , Npc/4
+    inv_Vc = cov_inv_n+cov_inv_t
+    Vc = np.linalg.inv(inv_Vc)
         
-        u_Vc, s_Vc, vh_Vc = np.linalg.svd(Vc)
-        u_inv_Vc, s_inv_Vc, vh_inv_Vc = np.linalg.svd(inv_Vc)
-        det_Vc, det_inv_Vc = np.prod(s_Vc), np.prod(s_inv_Vc)
+    u_Vc, s_Vc, vh_Vc = np.linalg.svd(Vc)
+    u_inv_Vc, s_inv_Vc, vh_inv_Vc = np.linalg.svd(inv_Vc)
+    det_Vc, det_inv_Vc = np.prod(s_Vc), np.prod(s_inv_Vc)
         
-        u_inv_Vn, s_inv_Vn, vh_inv_Vn = np.linalg.svd(Cov_Inv[label]['normal'])
-        u_inv_Ve, s_inv_Ve, vh_inv_Ve = np.linalg.svd(Cov_Inv[label]['tumor'])
+    u_inv_Vn, s_inv_Vn, vh_inv_Vn = np.linalg.svd(cov_inv_n)
+    u_inv_Ve, s_inv_Ve, vh_inv_Ve = np.linalg.svd(cov_inv_t)        
         
+    log_sum=sum(-math.log(s_inv_Vn[i])-math.log(s_inv_Ve[i])+math.log(s_inv_Vc[i]) for i in range(Npc))        
         
-        log_sum = sum( -math.log(s_inv_Vn[i]) - math.log(s_inv_Ve[i]) +math.log(s_inv_Vc[i])  for i in range(DIMENSION))
+    eta_n = np.matmul(cov_inv_n,mean_n)
+    eta_e = np.matmul(cov_inv_t,mean_t)
+    eta_c = eta_n + eta_e
         
+    mixed = np.matmul(np.transpose(eta_n),np.matmul(cov_mat_n,eta_n))\
+           +np.matmul(np.transpose(eta_e),np.matmul(cov_mat_t,eta_e))\
+           -np.matmul(np.transpose(eta_c),np.matmul(Vc,eta_c))\
         
-        eta_n = np.matmul(Cov_Inv[label]['normal'],mean[label]['normal'])
-        eta_e = np.matmul(Cov_Inv[label]['tumor'],mean[label]['tumor'])
-        eta_c = eta_n + eta_e
+    arg= -0.25*(Npc*math.log(2*math.pi) + log_sum + mixed)        
         
-        mixed = np.matmul(np.transpose(eta_n),np.matmul(covariance_matrix[label]['normal'],eta_n))\
-               +np.matmul(np.transpose(eta_e),np.matmul(covariance_matrix[label]['tumor'],eta_e))\
-               -np.matmul(np.transpose(eta_c),np.matmul(Vc,eta_c))\
-        
-        arg= -0.5*(DIMENSION*math.log(2*math.pi) + log_sum + mixed)
-        
-        
-        overlap[label]= math.pow(2,A)*math.pow(2*math.pi,B)*math.exp(arg)*math.pow(det_inv_Vc,-0.25)
-        log_overlap[label]=-A*math.log(2)-B*math.log(2*math.pi)-arg +0.25*math.log(det_inv_Vc)
+    overlap = math.pow(2,A)*math.pow(2*math.pi,B)*math.exp(arg)*math.pow(det_inv_Vc,-0.25)
+    log_overlap = -A*math.log(2)-B*math.log(2*math.pi)-arg +0.25*math.log(det_inv_Vc)
+
     return overlap, log_overlap
 
 
-
-def _complexity_map(mean, covariance_matrix ,DIMENSION, EXCLUSION_LIST):  
-    entropy_dict = _H_multiv_gaussian(covariance_matrix,DIMENSION)
-    
-    delta_H = _delta_H(entropy_dict)
-    overlap, log_overlap = _overlap(mean, covariance_matrix ,DIMENSION)
-    
-    x1,x2,x3,y,z= [],[],[],[],[]
-    
-    for label in log_overlap.keys():
-        if (label in EXCLUSION_LIST):
-            pass
-        else:
-            y.append(log_overlap[label])
-            x1.append(entropy_dict[label]['normal'])
-            x2.append(entropy_dict[label]['tumor'])
-            x3.append(delta_H[label])
-            z.append(label)
-        
-    
-    fig, ax = plt.subplots()
-    ax.scatter(x3, y, label=r'$\Delta S=S_{tumor}-S_{normal}$')
-    ax.scatter(x1, y, label=r'$S_{normal}$')
-    ax.scatter(x2, y, label=r'$S_{tumor}$')
-    np.savetxt('entropy_dat.dat', np.transpose([z,x1,x2,x3,y]),
-    header='tissue\tSnormal\t\t\tStumor\t\t\tdeltaS\t\t\t-ln(I)',comments='', delimiter="\t", fmt="%s")
-    
-    for i, txt in enumerate(z):
-        ax.annotate(txt, (x1[i], y[i]))
-        ax.annotate(txt, (x2[i], y[i]))
-        ax.annotate(txt, (x3[i], y[i]))
-    
-    ident = [0.0, 100.0]
-    fit= np.polyfit(x3,y,1)#Polynomial coefficients, highest power first
-    
-    fitted_y =[fit[0]*i+fit[1] for i in ident] 
-    
-    
-    plt.plot(ident,fitted_y,'b--',label=r"$m \approx {0},n \approx {1}$".format(np.around(fit[0], 2),np.around(fit[1], 2)),linewidth=1)
-    plt.xlabel(r"$\Delta S, S$ (nats)")
-    plt.ylim(0, 100)
-    plt.ylabel(r"$-\ln \,I$")
-    plt.title("Overlap $vs$ Entropy")
-    pylab.legend(loc='lower left')
-    plt.tight_layout()
-    plt.savefig('overlapentropy_fig.pdf')   
-    
-    return fit
-
-# The main program
+# The main program -----------------------------------------------------------------------------------------
 if __name__ == "__main__":
-    data, missmatch_pc_samples=read_data(srcpc,srcsampl)
-    core_matrix = _transpose_data(data)
+    random.seed(a=1234567890)
+    #random.seed()
+    N_normal,N_tumor=[],[]
+    S_normal,S_tumor,S_delta=[],[],[]
+    I,lnI=[],[]
+    S_normal_samp_mean,S_normal_samp_std,S_tumor_samp_mean,S_tumor_samp_std,S_delta_samp=[],[],[],[],[]
+    S_tumor_samp_min_mean,S_tumor_samp_min_std,S_delta_samp_min=[],[],[]
+    lnI_samp_mean,lnI_samp_std,lnI_samp_min_mean,lnI_samp_min_std=[],[],[],[]
+    Nn_min=23
+    for t_id in tissues_id: 
+        pc_normal,pc_tumor=read_PC(sample_path,PC_path,t_id,Npc)
+        N_normal.append(len(pc_normal))
+        N_tumor.append(len(pc_tumor))
+        print('normal/tumor:',len(pc_normal),len(pc_tumor))
+        print('PCs:',str(len(pc_normal[0]))+'x'+str(len(pc_tumor[0])))
 
-    EXCLUSION_LIST =['READ','ESCA','CESC','GBM','PAAD','BLCA']# BECAUSE FEW 'normal' SAMPLES, OTHERWISE EMPTY. 
+        Sn_i,St_i,lnI_i=[],[],[]
+        for i in range(N_random):
+            pc_normal_samp=pc_normal[random.sample(range(len(pc_normal)),Nn_min)]
+            pc_tumor_samp=pc_tumor[random.sample(range(len(pc_tumor)),len(pc_normal))]
+            S_n,mean_n,cov_mat_n,cov_inv_n=compute_entropy(pc_normal_samp)
+            S_t,mean_t,cov_mat_t,cov_inv_t=compute_entropy(pc_tumor_samp)
+            Sn_i.append(S_n)
+            St_i.append(S_t)
+            I_id,logI_id = compute_overlap(Npc,mean_n,mean_t,cov_mat_n,cov_mat_t,cov_inv_n,cov_inv_t)
+            lnI_i.append(logI_id)
+        S_normal_samp_mean.append(np.mean(Sn_i))
+        S_normal_samp_std.append(np.std(Sn_i))
+        S_tumor_samp_min_mean.append(np.mean(St_i))
+        S_tumor_samp_min_std.append(np.std(St_i))
+        lnI_samp_min_mean.append(np.mean(lnI_i))
+        lnI_samp_min_std.append(np.std(lnI_i))
 
-    _PC_pair_plot(core_matrix, 1, 2,'LUSC') 
 
-    low_DIMENSION_core_matrix=_low_dimension_core_matrix(core_matrix, LOW_DIMENSION=20)
-    low_DIMENSION_covariance_matrix = _covariance_matrix_(low_DIMENSION_core_matrix)
-    low_DIMENSION_mean = _mean_dict(low_DIMENSION_core_matrix)
-    low_DIMENSION_std = _std_dict(low_DIMENSION_core_matrix)
+        S_n,mean_n,cov_mat_n,cov_inv_n=compute_entropy(pc_normal)
+        S_normal.append(S_n)
+        S_t,mean_t,cov_mat_t,cov_inv_t=compute_entropy(pc_tumor)
+        S_tumor.append(S_t)
+        I_id,logI_id = compute_overlap(Npc,mean_n,mean_t,cov_mat_n,cov_mat_t,cov_inv_n,cov_inv_t)
+        I.append(I_id)
+        lnI.append(logI_id)
 
-    linear_fit =_complexity_map(low_DIMENSION_mean,  low_DIMENSION_covariance_matrix ,20, EXCLUSION_LIST)
+        S_i,lnI_i=[],[]
+        for i in range(N_random):
+            pc_tumor_samp=pc_tumor[random.sample(range(len(pc_tumor)),len(pc_normal))]
+            S_t,mean_t,cov_mat_t,cov_inv_t=compute_entropy(pc_tumor_samp)
+            S_i.append(S_t)
+            I_id,logI_id = compute_overlap(Npc,mean_n,mean_t,cov_mat_n,cov_mat_t,cov_inv_n,cov_inv_t)
+            lnI_i.append(logI_id)
+        S_tumor_samp_mean.append(np.mean(S_i))
+        S_tumor_samp_std.append(np.std(S_i))
+        lnI_samp_mean.append(np.mean(lnI_i))
+        lnI_samp_std.append(np.std(lnI_i))
 
+        S_delta.append(S_tumor[-1]-S_normal[-1])
+        S_delta_samp.append(S_tumor_samp_mean[-1]-S_normal[-1])
+        S_delta_samp_min.append(S_tumor_samp_min_mean[-1]-S_normal_samp_mean[-1])
+
+    np.savetxt('entropies_dat.dat', np.transpose([tissues_id,N_normal,N_tumor,S_normal,S_tumor,S_delta,
+                                    I,lnI]),
+    header='tissue\tNnormal\tNtumor\tSn\t\t\tSt\t\t\tdeltaS\t\t\tI\t\t\t-ln(I)',
+    comments='', delimiter=" \t", fmt="%s")
+    np.savetxt('entropies_samp-'+str(Nn_min)+'_dat.dat', np.transpose([tissues_id,N_normal,N_tumor,
+    S_normal_samp_mean,S_normal_samp_std,S_tumor_samp_min_mean,S_tumor_samp_min_std,S_delta_samp_min,
+    lnI_samp_min_mean,lnI_samp_min_std]),
+    header='tissue\tNnormal\tNtumor\t<Sn>_'+str(Nn_min)+'\t\t\tstd(Sn)_'+str(Nn_min)+'\t\t<St>_'+
+    str(Nn_min)+'\t\t\tstd(St)_'+str(Nn_min)+'\t\t<St>-<Sn>\t\t-<lnI>_'+str(Nn_min)+'\t\tstd(lnI)_'+
+    str(Nn_min),comments='',delimiter=" \t",fmt="%s")
+    np.savetxt('entropies_samp-Nn_dat.dat', np.transpose([tissues_id,N_normal,N_tumor,S_normal,
+    S_tumor_samp_mean,S_tumor_samp_std,S_delta_samp,lnI_samp_mean,lnI_samp_std]),
+    header='tissue\tNnormal\tNtumor\tSn\t\t\t<St>_Nn\t\t\tstd(St)_Nn\t\t<St>-Sn\t\t\t-<lnI>_Nn'+
+    '\t\tstd(lnI)_Nn',comments='',delimiter=" \t",fmt="%s")
+
+
+# plotting PC1 vs. PC2 ----------------------------
+    print('\nPloting PC1 vs. PC2')
+    t_id='LUSC'
+    pc_normal,pc_tumor=read_PC(sample_path,PC_path,t_id,2)
+    plt.scatter(pc_normal[:,0],pc_normal[:,1],label='normal',c='b',s=15)
+    plt.scatter(pc_tumor[:,0],pc_tumor[:,1],label='tumor',c='r',s=15)
+    plt.xlabel('PC1')
+    plt.ylabel('PC2')
+    plt.title(t_id)
+    plt.legend()
+    plt.tight_layout()
+    plt.grid(True)
+    plt.savefig('ge'+t_id+'_pc1-2_fig.pdf')
+    plt.clf()
+
+
+print("Done!")
